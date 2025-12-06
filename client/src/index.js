8b628707b189fd725b66a4d89532368d727b3509
Shravan.B.Patel <shravan.b.patel954@gmail.com>
2025-12-06 12:00:36 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -19,17 +19,20 @@ root.render(
   </React.StrictMode>
 );
 
-// 🔥 OPTION 1 FIX: Disable all service workers (prevents API interception on Render)
+// Register service worker for PWA
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    registrations.forEach((registration) => {
-      registration.unregister();
-    });
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered successfully:', registration.scope);
+      })
+      .catch((error) => {
+        console.log('Service Worker registration failed:', error);
+      });
   });
 }
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
-
-
