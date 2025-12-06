4cded1800ac39b56e908742909fde211588c437a
Shravan.B.Patel <shravan.b.patel954@gmail.com>
2025-12-06 16:19:40 +0530

    Change auth route to a broader API version route

--- backend/src/app.js
+++ backend/src/app.js
@@ -68,7 +68,8 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
 ======================= */
 
 // Auth
-app.use("/api/v1/auth", require("./router/index.router"));
+app.use("/api/v1", require("./router/index.router"));
+
 
 // Modules
 app.use("/api/v1/auth/modules", require("./router/module.router"));
